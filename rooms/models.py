f49c9b125a69629793e0e098bb82c60445434a32
Junwo Jeon <loust3598@naver.com>
2021-03-28 22:27:36 +0900

    updated

--- rooms/models.py
+++ rooms/models.py
@@ -87,7 +87,7 @@ class Room(core_models.TimeStampedModel):
         if len(all_reviews) > 0:
             for review in all_reviews:
                 all_ratings += review.rating_average()
-            return all_ratings / len(all_reviews)
+            return round(all_ratings / len(all_reviews), 1)
         return 0
 
 
