7026d9f693f09ab9dddbb52e40612c1a1cfc5901
Junwo Jeon <loust3598@naver.com>
2021-03-30 01:23:23 +0900

    updated

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-from django.utils import timezone
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -15,10 +13,11 @@ class HomeView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
+
         return context
 
 
-def room_detail(request, pk):
-    return render(request, "rooms/detail.html")
+class RoomDetail(DetailView):
+    """ Room Detail Definition """
+
+    model = models.Room
