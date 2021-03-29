233fba6a29099fd5b42782a437aa43be2677ceae
Junwo Jeon <loust3598@naver.com>
2021-03-30 02:57:38 +0900

    updated

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -21,3 +22,9 @@ class RoomDetail(DetailView):
     """ Room Detail Definition """
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
