207175aa1456a9b86b93e838ffbb7314372a8b5d
Junwo Jeon <loust3598@naver.com>
2021-03-31 01:59:57 +0900

    updated

--- rooms/models.py
+++ rooms/models.py
@@ -1,8 +1,7 @@
-from django.urls import reverse
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -19,15 +18,16 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
-    """ RoomType Object Definition """
+
+    """ RoomType Model Definition """
 
     class Meta:
         verbose_name = "Room Type"
-        ordering = ["-created"]
 
 
 class Amenity(AbstractItem):
-    """ Amenity Object Definition """
+
+    """ Amenity Model Definition """
 
     class Meta:
         verbose_name_plural = "Amenities"
@@ -37,18 +37,34 @@ class Facility(AbstractItem):
 
     """ Facility Model Definition """
 
+    pass
+
     class Meta:
         verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
+
     """ HouseRule Model Definition """
 
     class Meta:
         verbose_name = "House Rule"
 
 
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField(upload_to="room_photos")
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
 class Room(core_models.TimeStampedModel):
+
     """ Room Model Definition """
 
     name = models.CharField(max_length=140)
@@ -57,7 +73,7 @@ class Room(core_models.TimeStampedModel):
     city = models.CharField(max_length=80)
     price = models.IntegerField()
     address = models.CharField(max_length=140)
-    guests = models.IntegerField()
+    guests = models.IntegerField(help_text="How many people will be staying?")
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
@@ -65,21 +81,21 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(
-        user_models.User, related_name="rooms", on_delete=models.CASCADE
+        "users.User", related_name="rooms", on_delete=models.CASCADE
     )
     room_type = models.ForeignKey(
-        RoomType, related_name="rooms", on_delete=models.SET_NULL, null=True
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
     )
-    amenities = models.ManyToManyField(Amenity, related_name="rooms", blank=True)
-    facilities = models.ManyToManyField(Facility, related_name="rooms", blank=True)
-    house_rules = models.ManyToManyField(HouseRule, related_name="rooms", blank=True)
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
+
+    def __str__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
 
     def get_absolute_url(self):
         return reverse("rooms:detail", kwargs={"pk": self.pk})
@@ -90,17 +106,5 @@ class Room(core_models.TimeStampedModel):
         if len(all_reviews) > 0:
             for review in all_reviews:
                 all_ratings += review.rating_average()
-            return round(all_ratings / len(all_reviews), 1)
+            return round(all_ratings / len(all_reviews), 2)
         return 0
-
-
-class Photo(core_models.TimeStampedModel):
-
-    """ Photo Model Definition """
-
-    caption = models.CharField(max_length=80)
-    file = models.ImageField(upload_to="room_photos")
-    room = models.ForeignKey(Room, related_name="photos", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.caption
