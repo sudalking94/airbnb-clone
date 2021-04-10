3886e9811425d720cfb2dea1cb2614d772e32b98
Junwo Jeon <loust3598@naver.com>
2021-04-10 22:57:01 +0900

    updated

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "_!g2^hpkmtmuc*%hu$vq=s8hws3s6p%#d!de(5p571ug_!5rhr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
